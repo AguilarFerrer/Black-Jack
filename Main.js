a7bd3a50203bb2c88fcb71a3956adc90ba9aba45
AguilarFerrer <felixaguilarferrer@gmail.com>
2017-08-22 16:59:20 +0200

    Update Main js and html
    Working basic program.
     - Reworked function Count.
     - Added function CPU.
     - Added function ShowCard.
     - Deleted test from function GetCard.
    Updated html.
     - Added function Program.
     - Added inputs.
     - Added output.

--- Main.js
+++ Main.js
@@ -23,39 +23,46 @@ function GetCard(obj1, obj2){
     obj2.used = pos;
 }
 
-function Count(obj){ 
-        
-        if (parseInt(SubstrCard(obj, obj.card.length-1, 0, 2)) > 9){
-            obj.totalcard += 10;
-        }
-        else{
-            obj.totalcard += parseInt(SubstrCard(obj, obj.card.length-1, 0, 2));
-        }
-        
-        var ace = 0;
-        for (let i = 0; i < obj.card.length; i++){
-            if (parseInt(SubstrCard(obj, i, 0,2)) == 1){
-                ace++;
-            }
-        }
-        if (ace > 0){
-            if (obj.totalcard + 10 < 22){
-                obj.totalcard = obj.totalcard + 10;
-                obj.ace = true;
-            }
-            else if (obj.ace == true && obj.totalcard > 21){
-                obj.totalcard = obj.totalcard - 10;
-                obj.ace = false;
-            }
-            
-        }
-    
-    
-        //test
-        txt = "";
-        for (let i = 0; i < obj.card.length; i++){
-            txt += `${obj.card[i]}, `;
-        }
-        
-        document.getElementById("output").innerHTML = `${obj.totalcard} - ${txt}` ;
+function Count(obj){          
+    if (parseInt(SubstrCard(obj, obj.card.length-1, 0, 2)) > 9){
+        obj.totalcard += 10;
     }
+    else{
+        obj.totalcard += parseInt(SubstrCard(obj, obj.card.length-1, 0, 2));
+    }
+    var ace = 0;
+    for (let i = 0; i < obj.card.length; i++){
+        if (parseInt(SubstrCard(obj, i, 0,2)) == 1){
+            ace++;
+        }
+    }
+    if (ace > 0){
+        if (obj.totalcard + 10 < 22){
+            obj.totalcard = obj.totalcard + 10;
+            obj.ace = true;
+        }
+        else if (obj.ace == true && obj.totalcard > 21){
+            obj.totalcard = obj.totalcard - 10;
+            obj.ace = false;
+        }        
+    }
+}
+
+function CPU(obj1, obj2){
+    while(obj1.totalcard < 17){
+        GetCard(obj1, obj2);
+    }
+    ShowCard("crupier", obj1, obj1.card.length, true);
+}
+
+function ShowCard(out, obj, many, total){
+    var txt = "cartas";
+    for (let i = 0; i < many; i++){
+        txt += `${obj.card[i]} `;
+    }
+    if (total == true){
+        txt += `total: ${obj.totalcard}`;
+    }
+    
+    document.getElementById(`${out}`).innerHTML = txt;
+}
